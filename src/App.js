6943a79c0d345713be3c98403de6b8c1c73c9613
Fernando Arey <fernando.arey@rappi.com>
2023-02-27 08:29:48 -0600

    use reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { UseState } from './UseState';
-import { ClassState } from './ClassState';
+import { UseReducer } from './UseReducer';
 
 function App() {
   return (
     <div className="App">
-      <UseState name="UseState"/>
-      <ClassState name="ClassState"/>
+      <UseState name="Use State"/>
+      <UseReducer name="Use Reducer"/>
     </div>
   );
 }
--- src/useReducer.js
+++ src/useReducer.js
@@ -1,3 +1,68 @@
+import React from 'react';
+
+const SECURITY_CODE = 'paradigma';
+
+function UseReducer({ name }) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  React.useEffect(() => {
+    console.log('Empezando el efecto');
+    if(state.loading) {
+      setTimeout(() => {
+        console.log('Haciendo la validacion');
+        if (state.value === SECURITY_CODE) {
+          dispatch({
+            type: 'CONFIRM'
+          });
+        } else {
+          dispatch({
+            type: 'ERROR'
+          });
+        }
+        console.log('Terminando la validacion');
+      }, 2000);
+    }
+    console.log('Terminando el efecto');
+  }, [state.loading]);
+
+  if(!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar { name }</h2>
+        <p>Por favor, escribe el código de seguridad.</p>
+        {(state.error && !state.loading) && (
+          <p>Error: el código es incorrecto</p>
+        )}
+        {state.loading && (
+          <p>cargando...</p>
+        )}
+        <input 
+          value={state.value} 
+          onChange={(event) => dispatch({ type: 'WRITE', payload: event.target.value})}
+          placeholder="Código de seguridad"/>
+        <button
+          onClick={() => dispatch({ type: 'CHECK'})}
+        >Comprobar</button>
+      </div>
+    );
+  } else if(state.confirmed && !state.deleted) {
+    return(
+      <React.Fragment>
+        <p>¿Estas seguro de eliminar?</p>
+        <button onClick={() => { dispatch({ type: 'DELETE' }) }}>Si, eliminar</button>
+        <button onClick={() => { dispatch({ type: 'RESET' }) }}>No</button>
+      </React.Fragment>
+    );
+  } else {
+    return(
+      <React.Fragment>
+        <p>Eliminado con exito</p>
+        <button onClick={() => { dispatch({ type: 'RESET' }) }}>Volver atras</button>
+      </React.Fragment>
+    );
+  }
+}
+
 const initialState = {
   value: '',
   error: false,
@@ -6,48 +71,7 @@ const initialState = {
   confirmed: false,
 }
 
-// const reducer = (state, acction) => {
-// }
-
-
-const reducerIf = (state, acction) => {
-  if (acction.type === 'ERROR') {
-    return {
-      ...state,
-      error: true,
-      loading: false,
-    }
-  } else if (acction.type === 'CHECK') {
-    return {
-      ...state,
-      loading: true,
-    }
-  } else {
-    return { ...initialState };
-  }
-}
-
-const reducerSwitch = (state, acction) => {
-  switch(acction.type) {
-    case 'ERROR':
-      return {
-        ...state,
-        error: true,
-        loading: false,
-      }
-    case 'CHECK':
-      return {
-        ...state,
-        loading: true,
-      }
-    default:
-      return {
-        ...state
-      }
-  }
-}
-
-const reducerObject = (state) => ({
+const reducerObject = (state, payload) => ({
   'ERROR': {
     ...state,
     error: true,
@@ -56,13 +80,35 @@ const reducerObject = (state) => ({
   'CHECK': {
     ...state,
     loading: true,
+  },
+  'DELETE': {
+    ...state,
+    deleted: true,
+  }, 
+  'RESET': {
+    ...state,
+    confirmed: false,
+    deleted: false,
+    value: ''
+  },
+  'WRITE': {
+    ...state,
+    value: payload
+  },
+  'CONFIRM': {
+    ...state,
+    error: false,
+    loading: false,
+    confirmed: true,
   }
 });
 
 const reducer = (state, action) => {
   if (reducerObject(state)[action.type]) {
-    return reducerObject(state)[action.type];
+    return reducerObject(state, action.payload)[action.type];
   } else {
     return state
   }
 }
+
+export { UseReducer }
